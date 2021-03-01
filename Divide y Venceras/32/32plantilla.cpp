932170c3c2b0d8b0ec5c97f395cb2af7fffc96ff
Daniel Ledesma Ventura <61503116+DaniLedesma1@users.noreply.github.com>
2021-03-01 03:27:52 +0100

    Add files via upload

---
+++ Divide y Venceras/32/32plantilla.cpp
@@ -0,0 +1,78 @@
+//*****************
+// IMPORTANTE
+//
+// Nombre y apellidos del alumno
+// Usuario del juez de examen que has utilizado en la prueba de hoy
+//
+//***************************************************
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+//*******************************
+// Explicacion del algoritmo utilizado
+// Coste de la solucion. Recurrencia y orden de complejidad
+//*******************
+
+// Aqui la funcion recursiva que resuelve el problema
+
+
+int resolver(const std::vector<int>&datos, const std::vector<int> &datos2, int ini, int fin) {
+	int medio = (ini + fin) / 2;
+	if (ini == fin)
+	{
+		return datos[ini];
+	}
+	else
+	{
+		if (datos[medio] != datos2[medio]) return resolver(datos, datos2, ini, medio);
+		else return resolver(datos, datos2, medio + 1, fin);
+	}
+
+}
+
+// Tratar cada caso
+void resuelveCaso() {
+    int numA, sol;
+    // lectura del vector A
+    std::cin >> numA;
+    std::vector<int> a(numA);
+    for (int i = 0; i < numA; ++i)
+        std::cin >> a[i];
+    // lectura del vector B
+    std::vector<int> b(numA-1);
+    for (int j = 0; j < numA - 1; ++j)
+        std::cin >> b[j];
+    // resolver
+	if (numA == 1) 	sol = a[0];	
+	else if (a[0] != b[0]) 	sol = a[0];	
+	else if (a[a.size() - 1] != b[b.size() - 1]) sol = a[a.size() - 1];
+	else sol = resolver(a, b, 0, a.size() -1);
+	
+    // mostrar resultado
+    std::cout << sol << '\n';
+    
+}
+
+
+int main() {
+    // Para la entrada por fichero.
+#ifndef DOMJUDGE
+    std::ifstream in("32sample.in");
+    auto cinbuf = std::cin.rdbuf(in.rdbuf()); 
+#endif
+
+    int casos;
+    std::cin >> casos;
+    for (int i = 0; i < casos; ++i)
+        resuelveCaso();
+    
+    // Para restablecer entrada.
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    std::cin.rdbuf(cinbuf);
+    system("PAUSE");
+#endif
+    
+    return 0;
+}
---
+++ Divide y Venceras/33/Source.cpp
@@ -0,0 +1,78 @@
+//*****************
+// IMPORTANTE
+//
+// Nombre y apellidos del alumno
+// Usuario del juez de examen que has utilizado en la prueba de hoy
+//
+//***************************************************
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+//*******************************
+// Explicacion del algoritmo utilizado
+// Coste de la solucion. Recurrencia y orden de complejidad
+//*******************
+
+// Aqui la funcion recursiva que resuelve el problema
+
+
+void mergesort(std::vector <int> & v, int ini, int fin) {
+	if (ini + 1 < fin) { // Vector de mas de un elemento
+		int mitad = (ini + fin) / 2;
+		// Ordena cada parte
+		mergesort(v, ini, mitad);
+		mergesort(v, mitad, fin);
+		// mezcla
+		std::inplace_merge(v.begin() + ini, v.begin() + mitad, v.begin() + fin);
+	}
+}
+
+
+
+// Tratar cada caso
+bool resuelveCaso() {
+	int tam;
+
+	
+	std::cin >> tam;
+	if (!std::cin)
+		return false;
+	
+
+	std::vector<int> v(tam);
+	for (int i = 0; i < tam; ++i)
+		std::cin >> v[i];
+
+	// resolver
+	if (tam == 1) 	std::cout << v[0] << '\n';
+	else {
+		mergesort(v, 0, v.size()); //Ordeno el vector
+		std::cout << v[0] << '\n';
+	}
+	// mostrar resultado
+	
+	return true;
+}
+
+
+int main() {
+	// Para la entrada por fichero.
+#ifndef DOMJUDGE
+	std::ifstream in("33sample.in");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+	while (resuelveCaso());
+		
+
+	// Para restablecer entrada.
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
